833e151e78eaeec5c32c737e3a599d49e3d51913
kylej <kylej@bluewhaletech.pro>
2023-05-30 09:44:12 +0800

    0.0.3

--- HCDeviceInfo/Classes/HDDevice.h
+++ HCDeviceInfo/Classes/HDDevice.h
@@ -15,23 +15,6 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-extern NSString* const kCTSMSMessageReceivedNotification;
-extern NSString* const kCTSMSMessageReplaceReceivedNotification;
-extern NSString* const kCTSIMSupportSIMStatusNotInserted;
-extern NSString* const kCTSIMSupportSIMStatusReady;
-id CTTelephonyCenterGetDefault(void);
-void CTTelephonyCenterAddObserver(id,id,CFNotificationCallback,NSString*,void*,int);
-void CTTelephonyCenterRemoveObserver(id,id,NSString*,void*);
-int CTSMSMessageGetUnreadCount(void);
-int CTSMSMessageGetRecordIdentifier(void * msg);
-NSString * CTSIMSupportGetSIMStatus(void);
-NSString * CTSIMSupportCopyMobileSubscriberIdentity(void);
-id CTSMSMessageCreate(void* unknow/*always 0*/,NSString* number,NSString* text);
-void * CTSMSMessageCreateReply(void* unknow/*always 0*/,void * forwardTo,NSString* text);
-void* CTSMSMessageSend(id server,id msg);
-NSString *CTSMSMessageCopyAddress(void *, void *);
-NSString *CTSMSMessageCopyText(void *, void *);
-
 @interface HDDevice : NSObject
 
 @property (nonatomic, copy) NSString * token;
